590fe1cc5ca9243387222d6ae0cd2e1c52ad2353
simdev1234 <simdev1234@gmail.com>
2023-02-12 20:41:38 +0900

    - chore : 환경 설정 수정

---
+++ src/test/java/com/camping101/beta/BetaApplicationTest.java
@@ -0,0 +1,13 @@
+package com.camping101.beta;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BetaApplicationTest {
+
+    @Test
+    void main() {
+
+    }
+}
