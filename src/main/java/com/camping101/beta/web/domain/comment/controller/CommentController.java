e6b1cb74196d5bc38b5298cf5042a9bb370544cf
simdev1234 <simdev1234@gmail.com>
2023-05-23 20:12:06 +0900

    - refactor : 리팩토링

--- src/main/java/com/camping101/beta/web/domain/comment/controller/CommentController.java
+++ src/main/java/com/camping101/beta/web/domain/comment/controller/CommentController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
-
 import java.security.Principal;
 
 @RestController
